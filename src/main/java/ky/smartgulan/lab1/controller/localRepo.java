1b4cf0c0a5c4a6ef955ccca17bae570e6f3f1fd6
begzh <35567@iitu.edu.kz>
2024-10-10 19:02:08 +0500

    every change

---
+++ src/main/java/ky/smartgulan/lab1/controller/localRepo.java
@@ -0,0 +1,11 @@
+package ky.smartgulan.lab1.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/localRepo")
+public class localRepo {
+
+}
---
+++ src/main/java/ky/smartgulan/lab1/model/Student.java
@@ -0,0 +1,30 @@
+package ky.smartgulan.lab1.model;
+
+public class Student {
+
+    private int id;
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
